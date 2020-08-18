86234313c0c1ecdb6a8a5ab7d94f9a2cab4ba357
kezunren <kezunren@itouchtv.cn>
2020-08-18 21:15:22 +0800

    add terser plugin

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -2,10 +2,32 @@ const webpack = require('webpack');
 const path = require('path');
 const base = require('./webpack.config.base');
 const { merge } = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(base, {
     mode: 'production',
     entry: {
         app: [path.resolve(__dirname, '../src/index.js')],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true,
+                parallel: false,
+                terserOptions: {
+                    ecma: 8,
+                    output: {
+                        comments: false, // 保留注释
+                        beautify: false, // 不需要格式化
+                    },
+                    compress: {
+                        drop_console: true, // 去除console
+                        collapse_vars: true, // 内嵌定义了但是只有用到一次的变量
+                        reduce_vars: true, // 提取出出现多次但是没有定义成变量去引用的静态值
+                    },
+                },
+            }),
+        ],
     },
 });
